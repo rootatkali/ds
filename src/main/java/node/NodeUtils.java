92d04815832380caf0a6aff79dfab43788c26af3
Rotem <rootatkali4@gmail.com>
2020-12-09 13:36:01 +0200

    aa

--- src/main/java/node/NodeUtils.java
+++ src/main/java/node/NodeUtils.java
@@ -1,4 +1,5 @@
 package node;
+
 import DS.Node;
 
 import java.util.Objects;
@@ -20,6 +21,14 @@ public class NodeUtils {
       ret += head;
     }
     return ret;
+  }
+  
+  public static <T> Node<T> getLast(Node<T> head) {
+    while (head.hasNext()) {
+      head = head.getNext();
+    }
+    
+    return head;
   }
   
   public static <T> int size(Node<T> head) {
@@ -56,11 +65,118 @@ public class NodeUtils {
     lst.setNext(aft);
   }
   
+  public static Node<Integer> findMinP(Node<Integer> lst) {
+    Node<Integer> min = lst;
+    while (lst.hasNext()) {
+      if (lst.getNext().getValue() < min.getValue()) {
+        min = lst.getNext();
+      }
+      lst = lst.getNext();
+    }
+    return min;
+  }
+  
+  public static int findMinV(Node<Integer> lst) {
+    return findMinP(lst).getValue();
+  }
+  
+  public static <T> Node<T> insertBefore(Node<T> fst, Node<T> be, Node<T> pt) {
+    boolean ins = false;
+    if (fst == be) {
+      pt.setNext(fst);
+      return pt;
+    }
+    
+    Node<T> head = fst;
+    while (!ins && fst.hasNext()) {
+      if (fst.getNext() == be) {
+        ins = true;
+        pt.setNext(be);
+        fst.setNext(pt);
+      }
+      fst = fst.getNext();
+    }
+    return head;
+  }
+  
+  public static <T> Node<T> remove(Node<T> fst, Node<T> pt) {
+    Node<T> head = fst;
+    if (fst == pt) {
+      return fst.getNext();
+    }
+    while (fst != null) {
+      if (fst.getNext() == pt) {
+        fst.setNext(pt.getNext());
+      }
+      fst = fst.getNext();
+    }
+    return head;
+  }
+  
+  public static Node<Integer> sort(Node<Integer> fst) {
+    Node<Integer> head = findMinP(fst);
+    fst = remove(fst, findMinP(fst));
+    Node<Integer> last = head;
+    while (fst != null) {
+      last.setNext(findMinP(fst));
+      last = last.getNext();
+      fst = remove(fst, findMinP(fst));
+    }
+    
+    return head;
+  }
+  
+  public static <T> Node<T> mergeNotSorted(Node<T> a, Node<T> b) {
+    getLast(a).setNext(b);
+    return a;
+  }
+  
+  public static Node<Integer> mergeSorted(Node<Integer> a, Node<Integer> b) {
+    Node<Integer> head;
+    if (a.getValue() < b.getValue()) {
+      head = a;
+      a = a.getNext();
+    } else {
+      head = b;
+      b = b.getNext();
+    }
+    
+    Node<Integer> last = head;
+    
+    while (a != null && b != null) {
+      if (a.getValue() < b.getValue()) {
+        last.setNext(a);
+        a = a.getNext();
+      } else {
+        last.setNext(b);
+        b = b.getNext();
+      }
+    }
+    
+    if (a == null) return mergeNotSorted(head, b);
+    return mergeNotSorted(head, a);
+  }
+  
+  public static void removeDups(Node<Integer> fst) {
+    while (fst != null && fst.hasNext()) {
+      if (fst.getValue() == fst.getNext().getValue()) {
+        fst.setNext(fst.getNext().getNext());
+      } else fst = fst.getNext();
+    }
+  }
+  
   public static Node<Integer> randList(int length, int from, int to) {
     Integer[] arr = new Integer[length];
     for (int i = 0; i < length; i++) {
       arr[i] = (int) (Math.random() + (to - from + 1)) + to;
     }
     return newFrom(arr);
+  }
+  
+  public static void main(String[] args) {
+    Node<Integer> lst = newFrom(1, 5, 2, 3, 4, 4, 6, 4);
+    System.out.println(toString(sort(lst)));
+    removeDups(lst);
+    System.out.println(toString(lst));
   }
 }
