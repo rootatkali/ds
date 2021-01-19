313f5f4996031ea92488d692148d894f6f9fcafe
Rotem <rotem@faceco.de>
2021-01-19 14:28:11 +0200

    fix error with isPrime

--- src/main/java/node/QueueNode.java
+++ src/main/java/node/QueueNode.java
@@ -8,6 +8,7 @@ public class QueueNode {
   private static boolean isPrime(int n) {
     if (n <= 0) return false;
     if (n == 1 || n == 2) return true;
+    if (n % 2 == 0) return false;
     
     for (int i = 3; i <= n / 2; i+= 2) {
       if (n % i == 0) return false;
