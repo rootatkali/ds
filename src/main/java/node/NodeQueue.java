f615fe4b086661aefe29c95c4c450d021ddddfd9
Rotem <rotem@faceco.de>
2021-01-19 14:55:18 +0200

    ugh 1 is not a prime

--- src/main/java/node/NodeQueue.java
+++ src/main/java/node/NodeQueue.java
@@ -6,8 +6,8 @@ import queue.QueueUtils;
 
 public class NodeQueue {
   private static boolean isPrime(int n) {
-    if (n <= 0) return false;
-    if (n == 1 || n == 2) return true;
+    if (n <= 1) return false;
+    if (n == 2) return true;
     if (n % 2 == 0) return false;
     
     for (int i = 3; i <= n / 2; i+= 2) {
